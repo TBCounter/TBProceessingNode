d37f1dc4a18a7e48ea01ec23508ebc86c0152875
Ivan Riazanov <omega19961@gmail.com>
2024-06-01 20:46:45 +0600

    added empty database

---
+++ database.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
--- sockets.js
+++ sockets.js
@@ -1,30 +1,44 @@
 // client.js
 const io = require('socket.io-client');
 
+const db = require('./database')
+
+
+// синхронизация дб после запуска
+db.sequelize.sync({ force: true })
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err) => {
+        console.log("Failed to sync db: " + err.message);
+    });
+
+
+
 // Адрес сервера
 const socket = io('http://localhost:3000', {
-  transports: ['websocket'], // Использование WebSocket транспорта
+    transports: ['websocket'], // Использование WebSocket транспорта
 });
 
 // Событие успешного подключения
 socket.on('connect', () => {
-  console.log('Connected to server');
+    console.log('Connected to server');
 
-  // Можно отправлять сообщения на сервер
-  socket.emit('message', 'Hello from client!');
+    // Можно отправлять сообщения на сервер
+    socket.emit('message', 'Hello from client!');
 });
 
 // Событие получения сообщения от сервера
 socket.on('message', (data) => {
-  console.log('Received message from server:', data);
+    console.log('Received message from server:', data);
 });
 
 // Событие отключения
 socket.on('disconnect', () => {
-  console.log('Disconnected from server');
+    console.log('Disconnected from server');
 });
 
 // Событие ошибки
 socket.on('connect_error', (error) => {
-  console.error('Connection error:', error);
+    console.error('Connection error:', error);
 });
