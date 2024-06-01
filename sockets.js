f9bb0c7f86adb2d5037c007c5e29da8268b659c3
Ivan Riazanov <omega19961@gmail.com>
2024-06-01 22:50:40 +0600

    added launching via login and password

--- sockets.js
+++ sockets.js
@@ -1,8 +1,7 @@
 // client.js
 const io = require('socket.io-client');
-
 const db = require('./database')
-
+const playwright = require('playwright');
 
 // синхронизация дб после запуска
 db.sequelize.sync({ force: true })
@@ -16,7 +15,7 @@ db.sequelize.sync({ force: true })
 
 
 // Адрес сервера
-const socket = io('http://localhost:3000', {
+const socket = io('http://localhost:3000/node', {
     transports: ['websocket'], // Использование WebSocket транспорта
 });
 
@@ -42,3 +41,29 @@ socket.on('disconnect', () => {
 socket.on('connect_error', (error) => {
     console.error('Connection error:', error);
 });
+
+
+
+socket.on('run_account', async (payload) => {
+    const browser = await playwright['chromium'].launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto(payload.address);
+    // await page.waitForSelector('#registration > div.popup-stretch__content > div > form > div.aligncenter.t1_14.mb10 > span');
+    await page.screenshot({ path: `1.png`, fullPage: true });
+    const login_btn = page.locator('#registration > div.popup-stretch__content > div > form > div.aligncenter.t1_14.mb10 > span')
+    await login_btn.click();
+
+    const login = page.locator("#login > div.popup-stretch__content > form > div:nth-child(2) > div > input[type=email]")
+    await login.fill(payload.login)
+    const password = page.locator("#login > div.popup-stretch__content > form > div:nth-child(3) > div > input[type=password]")
+    await password.fill(payload.password)
+
+    const login_button = page.locator('#login > div.popup-stretch__content > form > div:nth-child(4) > button')
+    await login_button.click()
+    await page.screenshot({ path: `nodejs_chromium.png`, fullPage: true });
+
+    // await browser.close().then(() => {
+    //     console.log('browser closed')
+    // });
+})
